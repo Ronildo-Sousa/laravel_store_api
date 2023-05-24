54badd17d46256fec17ea0975ca48a60145af5f3
Ronildo Sousa <sousaronildo400@gmail.com>
2023-05-24 20:33:15 -0300

    feat: updates user migration and factory

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -1,4 +1,6 @@
-<?php declare(strict_types = 1);
+<?php
+
+declare(strict_types = 1);
 
 namespace Database\Factories;
 
@@ -18,8 +20,10 @@ class UserFactory extends Factory
     public function definition(): array
     {
         return [
-            'name'              => fake()->name(),
+            'first_name'        => fake()->firstname(),
+            'last_name'         => fake()->lastName(),
             'email'             => fake()->unique()->safeEmail(),
+            'is_admin'          => fake()->boolean(),
             'email_verified_at' => now(),
             'password'          => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
             'remember_token'    => Str::random(10),
