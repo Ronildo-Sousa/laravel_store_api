3dbfc193a8bb73de7504344d28c19189650f476d
Ronildo Sousa <sousaronildo400@gmail.com>
2023-05-24 21:04:17 -0300

    tests: adds test validation for register a user

--- tests/Feature/User/RegisterTest.php
+++ tests/Feature/User/RegisterTest.php
@@ -2,6 +2,8 @@
 
 declare(strict_types = 1);
 
+use App\Models\User;
+
 use function Pest\Laravel\{assertDatabaseHas, postJson};
 
 it('should be able to register a user', function () {
@@ -21,4 +23,66 @@ it('should be able to register a user', function () {
         'email'    => 'john.doe@example.com',
         'is_admin' => false,
     ]);
+});
+
+test('first name is required', function () {
+    postJson(route('api.auth.register'), [
+        'first_name'            => '',
+        'last_name'             => 'Doe',
+        'email'                 => 'john.doe@example.com',
+        'password'              => 'password',
+        'password_confirmation' => 'password',
+    ])
+        ->assertUnprocessable()
+        ->assertJsonValidationErrors('first_name');
+});
+
+test('last name is required', function () {
+    postJson(route('api.auth.register'), [
+        'first_name'            => 'John',
+        'last_name'             => '',
+        'email'                 => 'john.doe@example.com',
+        'password'              => 'password',
+        'password_confirmation' => 'password',
+    ])
+        ->assertUnprocessable()
+        ->assertJsonValidationErrors('last_name');
+});
+
+test('email is required', function () {
+    postJson(route('api.auth.register'), [
+        'first_name'            => 'John',
+        'last_name'             => 'Doe',
+        'email'                 => '',
+        'password'              => 'password',
+        'password_confirmation' => 'password',
+    ])
+        ->assertUnprocessable()
+        ->assertJsonValidationErrors('email');
+});
+
+test('email must be unique', function () {
+    User::factory()->create(['email' => 'john.doe@example.com']);
+
+    postJson(route('api.auth.register'), [
+        'first_name'            => 'John',
+        'last_name'             => 'Doe',
+        'email'                 => 'john.doe@example.com',
+        'password'              => 'password',
+        'password_confirmation' => 'password',
+    ])
+        ->assertUnprocessable()
+        ->assertJsonValidationErrors('email');
+});
+
+test('password is required', function () {
+    postJson(route('api.auth.register'), [
+        'first_name'            => 'John',
+        'last_name'             => 'Doe',
+        'email'                 => 'john.doe@example.com',
+        'password'              => '',
+        'password_confirmation' => '',
+    ])
+        ->assertUnprocessable()
+        ->assertJsonValidationErrors('password');
 });
